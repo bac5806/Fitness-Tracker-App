ebe32a50274f5b05082f644286e1cf6f8e2bd365
Broc C <bac5806@gmail.com>
2021-06-15 12:07:31 -0400

    setting up db

--- models/workout.js
+++ models/workout.js
@@ -4,14 +4,33 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
   {
+    day: {
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    },
+    type: {
+      type: String,
+      trim: true,
+      required: "Enter the type of workout"
+    },
     name: {
       type: String,
       trim: true,
       required: "Enter name for workout"
     },
-    date: {
-      type: Date,
-      default: Date.now
+    duration: {
+      type: Number,
+      required: "Enter a duration for the workout"
+    },
+    reps: {
+      type: Number,
+      required: "Enter number of reps"
+    },
+    sets: {
+      type: Number,
+      required: "Enter number of sets"
     }
   }
 );
